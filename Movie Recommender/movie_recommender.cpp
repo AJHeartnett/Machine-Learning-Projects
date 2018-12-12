ff1ef30457dff14af4d6498f459866dde6dae66b
Anirudh Janagam <anirudh.goku0@gmail.com>
2018-12-13 00:02:42 +0100

    Update movie_recommender.cpp

--- Movie Recommender/movie_recommender.cpp
+++ Movie Recommender/movie_recommender.cpp
@@ -2,3 +2,5 @@ Build a Movie Recommender App in c++
 
 Build an app that can recommend movies you'd like.
 #Using Amazon's machine learning library called Deep Scalable Sparse Tensor Network Engine (DSSTNE).
+
+Train a neural network on a MovieLens dataset of movie ratings by different users to generate a top 10 recommendation list for the default user ID. 
